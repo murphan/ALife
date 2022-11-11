ea2a5546ea88cd7bba74357de839dda35c12b288
murphan <murphan@users.noreply.github.com>
2022-11-10 18:06:54 -0800

    Basic Simulation Controller class

---
+++ src/Environment/SimulationController.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Anna on 11/9/2022.
+//
+
+#include "src/Environment/SimulationController.h"
+#include <iostream>
+
+namespace std {
+    SimulationController::SimulationController() {
+        std::cout << "Sim Controller" << endl;
+    }
+} // std
---
+++ src/Environment/SimulationController.h
@@ -0,0 +1,20 @@
+//
+// Created by Anna on 11/9/2022.
+//
+
+#ifndef ALIFE_SIMULATIONCONTROLLER_H
+#define ALIFE_SIMULATIONCONTROLLER_H
+
+#include "src/Environment/Environment.h"
+
+namespace std {
+
+    class SimulationController {
+        public:
+            SimulationController();
+            //Environment environment;
+    };
+
+} // std
+
+#endif //ALIFE_SIMULATIONCONTROLLER_H
