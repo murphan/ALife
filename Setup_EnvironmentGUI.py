e5c97f7c066e7d6080c10082c50436b3939fe7a6
hudkinsnoah <81835412+hudkinsnoah@users.noreply.github.com>
2023-01-18 21:02:09 -0800

    Creating Data Processing GUI initial (#1)
    * Creating Data Processing GUI initial
    
    This is the first commit of the Data Processing GUI. There is currently only the right half of the screen displayed which is the two graphs and their dropdowns that they need. Due to there not being anything filled in on the left side for the genetic information (coming soon) everything is spaced closer to center. This will be fixed and respaced once more is added.
    
    * New Files and GUI/control Logic
    
    Just saving changes up to this point. New files added for the environment GUI and data processing and environment control. Still several changes needed for adding in functionality and making the GUI look cleaner, add in code comments, and set some boundaries to account for differences in screen sizes.
    
    * Environment Changes
    
    Adding in temporary changes to the environment for both set up and manipulation. Likely to be some change but this is at least the initial idea for the environment for both looks and manipulation
    
    * Secondary idea for Environment
    
    This is a second file that has other ideas for the Environment and how that will be set up and displayed. A lot is commented out just because of it being experimental and playing around with some manipulation of things at the moment.
    
    * New ideas and options
    
    This is saving changes to old files that have been updated and adding in likely the new and final option for creating a grid and buttons in the setup_environmentGUI.py file
    
    * Saving Changes
    
    Saving changes to repo
    
    * Initial GUI creation complete
    
    This is the initial look that the GUI will have. This has the very basics and still has a long ways to go, but it has all pages created, the starting of each of the elements, and all buttons are connected with a function of sorts.
    
    * Small changes and additions
    
    This adds in the ability to fill a square when clicked and updating some docstrings
    
    * Changes for click type
    
    This will allow the user to select a type of   block that is pressed. It hasn't been hooked to the Environment yet, but it does allow the user to select an option still.

---
+++ Setup_EnvironmentGUI.py
@@ -0,0 +1,97 @@
+import pygame
+import sys
+
+import Control_EnvironmentGUI as Control_Environment
+import Setup_settingsGUI
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+WINDOW_HEIGHT = 770
+WINDOW_WIDTH = 1500
+SCREEN = None
+CLOCK = None
+
+
+class SetupEnvironment:
+    """
+    This will set up the actual environment for the simulation to be displayed in
+    """
+    def __init__(self):
+        global SCREEN, CLOCK
+        pygame.init()
+        SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        CLOCK = pygame.time.Clock()
+        SCREEN.fill(WHITE)
+
+        # The three buttons that are on the environment screen
+        self.settings_button = Button(SCREEN, (1424, 738), "Settings", (255, 255, 0))
+        self.pause_button = Button(SCREEN, (1364, 738), "Pause", (255, 0, 0))
+        self.play_button = Button(SCREEN, (1320, 738), "Play", (0, 255, 0))
+
+        self.environment_grid = [[0] * int(WINDOW_HEIGHT / 10) for i in range(WINDOW_WIDTH)]
+
+        # TODO: Likely need to create instances of the setup_settings and setup_dataprocessing GUI's
+        # So that I can access each of them and pass settings along through pages and changes of states
+
+        while True:
+            self.drawGrid()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if self.settings_button.mouse_click(event):
+                    Setup_settingsGUI.SetupSettings()
+                elif self.play_button.mouse_click(event):
+                    Control_Environment.EnvironmentControl.start(self)
+                elif self.pause_button.mouse_click(event):
+                    Control_Environment.EnvironmentControl.stop(self)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    Control_Environment.EnvironmentControl.square_clicked(self, event, SCREEN)
+                # TODO: Likely will have to have another call here to update data and position of organisms
+
+            pygame.display.update()
+
+    def drawGrid(self):
+        """
+        This will draw the grid of the environment and initialize a two-dimensional array
+        """
+        global SCREEN
+        blockSize = 10
+        for x in range(0, WINDOW_WIDTH, blockSize):
+            for y in range(0, WINDOW_HEIGHT, blockSize):
+                if x >= WINDOW_WIDTH - 180 and y >= WINDOW_HEIGHT - 35:
+                    continue
+                rect = pygame.Rect(x, y, blockSize, blockSize)
+                pygame.draw.rect(SCREEN, BLACK, rect, 1)
+
+
+class Button:
+    """
+    The button class which defines the buttons in the environment
+    """
+    def __init__(self, screen, position, text, color):
+        font = pygame.font.SysFont("Arial", 25)
+        text_options = font.render(text, True, (0, 0, 0))
+        x, y, w, h = text_options.get_rect()
+        x, y = position
+        self.button_rect = pygame.draw.rect(screen, color, (x, y, w, h))
+        screen.blit(text_options, self.button_rect)
+
+    def clicked(self):
+        """
+        Returns True if you clicked on the specified button being checked
+        """
+        x, y = pygame.mouse.get_pos()
+        return True if self.button_rect.collidepoint(x, y) else False
+
+    def mouse_click(self, event):
+        """
+        Checks if you click the mouse button and then if it's on the button
+        """
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                return True if self.clicked() else False
+
+
+if __name__ == "__main__":
+    SetupEnvironment()
