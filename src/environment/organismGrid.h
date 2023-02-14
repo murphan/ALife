b443374dbabc633bb3a9d2b3807fb266689ea829
balduvian <emmettglaser@gmail.com>
2023-02-14 10:01:48 -1000

    initial implementation of organismGrid

--- src/environment/organismGrid.h
+++ src/environment/organismGrid.h
@@ -8,12 +8,28 @@
 #include <vector>
 #include "types.h"
 
+#include "organism.h"
+
 class OrganismGrid {
 private:
+	i32 width, height;
 	std::vector<i32> grid;
 
-public:
+	auto inBounds(i32 x, i32 y) -> bool;
+	auto indexOf(i32 x, i32 y) -> i32;
 
+	auto internalSpaceAvailable(const Body & body, i32 tempId, i32 centerX, i32 centerY) -> bool;
+
+public:
+	OrganismGrid(i32 width, i32 height);
+
+	auto placeOrganism(Organism & organism, i32 tempId) -> void;
+
+	auto canMoveOrganism(Organism & organism, i32 tempId, i32 deltaX, i32 deltaY) -> bool;
+
+	auto moveOrganism(Organism & organism, i32 tempId, i32 deltaX, i32 deltaY) -> void;
+
+	auto isSpaceAvailable(const Body & body, i32 x, i32 y) -> bool
 };
 
 #endif //ALIFE_ORGANISMGRID_H
